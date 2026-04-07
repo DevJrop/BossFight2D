18711ca1e685db2df59578de8da4025af6f90092
Juan Rodriguez <juanjrop002@gmail.com>
2026-04-07 04:23:16 +0200

    Simple dash script on PlayerMove

--- Assets/Project/Scripts/Player/Movement/PlayerMove.cs
+++ Assets/Project/Scripts/Player/Movement/PlayerMove.cs
@@ -1,5 +1,5 @@
+using System.Collections;
 using UnityEngine;
-
 namespace Project.Scripts.Player.Movement
 {
     public class PlayerMove : MonoBehaviour
@@ -7,24 +7,32 @@ namespace Project.Scripts.Player.Movement
         [SerializeField] private float speed = 5f;
 
         private Rigidbody2D rb;
-        private Animator animation;
+        private Animator anim;
 
         private Vector2 moveInput;
         private Vector2 lastMove;
+        private bool isDashing;
 
+        [SerializeField]private float dashForce;
+        [SerializeField]private float dashTime;
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
-            animation = GetComponent<Animator>();
+            anim = GetComponent<Animator>();
+            isDashing = !isDashing;
         }
-
         private void Update()
         {
             InputMovement();
             UpdateAnimations();
             HandleFlip();
+            Dodge();
         }
-
+        private void FixedUpdate()
+        {
+            if (!isDashing) return;
+            rb.linearVelocity = moveInput * speed;
+        }
         private void InputMovement()
         {
             float moveX = Input.GetAxisRaw("Horizontal");
@@ -37,15 +45,12 @@ namespace Project.Scripts.Player.Movement
                 lastMove = moveInput;
             }
         }
-
         private void UpdateAnimations()
         {
-            animation.SetFloat("Horizontal", lastMove.x);
-            animation.SetFloat("Vertical", lastMove.y);
-            
-            animation.SetFloat("Speed", moveInput.magnitude);
+            anim.SetFloat("Horizontal", lastMove.x);
+            anim.SetFloat("Vertical", lastMove.y);
+            anim.SetFloat("Speed", moveInput.magnitude);
         }
-
         private void HandleFlip()
         {
             if (moveInput.x != 0)
@@ -53,10 +58,19 @@ namespace Project.Scripts.Player.Movement
                 transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1f, 1f);
             }
         }
-
-        private void FixedUpdate()
+        private void Dodge()
         {
-            rb.linearVelocity = moveInput * speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isDashing = false;
+                StartCoroutine(Dash());
+            }
+        }
+        IEnumerator Dash()
+        {
+            rb.linearVelocity = moveInput * dashForce;
+            yield return new WaitForSeconds(dashTime);
+            isDashing = true;
         }
     }
 }
