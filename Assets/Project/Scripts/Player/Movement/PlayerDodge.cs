907c2a20b27e17bb4dbdc142df11d90904f5b96d
Juan Rodriguez <juanjrop002@gmail.com>
2026-04-07 19:49:56 +0200

    Transfer dash to its own script, separating logics of movement and dodge

---
+++ Assets/Project/Scripts/Player/Movement/PlayerDodge.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+namespace Project.Scripts.Player.Movement
+{
+    public class PlayerDodge : MonoBehaviour
+    {
+        [SerializeField]private float dashForce;
+        [SerializeField]private float dashTime;
+        public bool isDashing;
+        PlayerMove playerMove;
+        private Rigidbody2D rb;
+        void Start()
+        {
+            playerMove = GetComponent<PlayerMove>();
+            rb = GetComponent<Rigidbody2D>();
+            isDashing = !isDashing;
+        }
+        void Update()
+        {
+            DodgeInput();
+        }
+        private void DodgeInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isDashing = false;
+                StartCoroutine(Dash());
+            }
+        }
+        IEnumerator Dash()
+        {
+            rb.linearVelocity = playerMove.moveInput * dashForce;
+            yield return new WaitForSeconds(dashTime);
+            isDashing = true;
+        }
+    }
+}
--- Assets/Project/Scripts/Player/Movement/PlayerMove.cs
+++ Assets/Project/Scripts/Player/Movement/PlayerMove.cs
@@ -1,45 +1,36 @@
-using System.Collections;
 using UnityEngine;
 namespace Project.Scripts.Player.Movement
 {
     public class PlayerMove : MonoBehaviour
     {
         [SerializeField] private float speed = 5f;
-
         private Rigidbody2D rb;
         private Animator anim;
-
-        private Vector2 moveInput;
+        public Vector2 moveInput;
         private Vector2 lastMove;
-        private bool isDashing;
-
-        [SerializeField]private float dashForce;
-        [SerializeField]private float dashTime;
+        PlayerDodge playerDodge;
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             anim = GetComponent<Animator>();
-            isDashing = !isDashing;
+            playerDodge = GetComponent<PlayerDodge>();
         }
         private void Update()
         {
             InputMovement();
             UpdateAnimations();
             HandleFlip();
-            Dodge();
         }
         private void FixedUpdate()
         {
-            if (!isDashing) return;
+            if(!playerDodge.isDashing) return;
             rb.linearVelocity = moveInput * speed;
         }
         private void InputMovement()
         {
             float moveX = Input.GetAxisRaw("Horizontal");
             float moveY = Input.GetAxisRaw("Vertical");
-
             moveInput = new Vector2(moveX, moveY).normalized;
-            
             if (moveInput != Vector2.zero)
             {
                 lastMove = moveInput;
@@ -57,20 +48,6 @@ namespace Project.Scripts.Player.Movement
             {
                 transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1f, 1f);
             }
-        }
-        private void Dodge()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                isDashing = false;
-                StartCoroutine(Dash());
-            }
-        }
-        IEnumerator Dash()
-        {
-            rb.linearVelocity = moveInput * dashForce;
-            yield return new WaitForSeconds(dashTime);
-            isDashing = true;
         }
     }
 }
