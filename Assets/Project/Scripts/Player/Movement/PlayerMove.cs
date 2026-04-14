df3d5b2d7236c7b5e78e1f6642d40e44ecd2d64d
Juan Rodriguez <juanjrop002@gmail.com>
2026-04-14 04:30:56 +0200

    Run animations, aceleration and deceleration.

--- Assets/Project/Scripts/Player/Movement/PlayerMove.cs
+++ Assets/Project/Scripts/Player/Movement/PlayerMove.cs
@@ -4,10 +4,18 @@ namespace Project.Scripts.Player.Movement
     public class PlayerMove : MonoBehaviour
     {
         [SerializeField] private float speed = 5f;
+        
+        [SerializeField] private float acceleration = 10f;
+        [SerializeField] private float deceleration = 8f;
+
+        private float currentSpeed;
+
         private Rigidbody2D rb;
         private Animator anim;
+
         public Vector2 moveInput;
         private Vector2 lastMove;
+
         PlayerDodge playerDodge;
         private void Awake()
         {
@@ -18,33 +26,49 @@ namespace Project.Scripts.Player.Movement
         private void Update()
         {
             InputMovement();
+            HandleSpeed(); 
             UpdateAnimations();
             HandleFlip();
         }
         private void FixedUpdate()
         {
-            if(!playerDodge.isDashing) return;
+            if (!playerDodge.isDashing) return;
+
             rb.linearVelocity = moveInput * speed;
         }
         private void InputMovement()
         {
             float moveX = Input.GetAxisRaw("Horizontal");
             float moveY = Input.GetAxisRaw("Vertical");
+
             moveInput = new Vector2(moveX, moveY).normalized;
+
             if (moveInput != Vector2.zero)
             {
                 lastMove = moveInput;
+            }
+        }
+        private void HandleSpeed()
+        {
+            if (moveInput != Vector2.zero)
+            {
+                currentSpeed = Mathf.MoveTowards(currentSpeed, 1f, acceleration * Time.deltaTime);
+            }
+            else
+            {
+                currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.deltaTime);
             }
         }
         private void UpdateAnimations()
         {
             anim.SetFloat("Horizontal", lastMove.x);
             anim.SetFloat("Vertical", lastMove.y);
-            anim.SetFloat("Speed", moveInput.magnitude);
+            anim.SetFloat("Speed", currentSpeed);
         }
         private void HandleFlip()
         {
             SpriteRenderer spritePlayer = GetComponent<SpriteRenderer>();
+
             if (moveInput.x < 0)
             {
                 spritePlayer.flipX = true;
@@ -54,7 +78,6 @@ namespace Project.Scripts.Player.Movement
             {
                 spritePlayer.flipX = false;
             }
-            return;
         }
     }
 }
