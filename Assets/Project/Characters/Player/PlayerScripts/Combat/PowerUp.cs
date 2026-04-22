5008d02cbf6816be0ebc3580744d4ee5d25a240c
Juan Rodriguez <juanjrop002@gmail.com>
2026-04-22 06:43:16 +0200

    PowerUp bug solved

--- Assets/Project/Characters/Player/PlayerScripts/Combat/PowerUp.cs
+++ Assets/Project/Characters/Player/PlayerScripts/Combat/PowerUp.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+
 namespace Project.Characters.Player.PlayerScripts.Combat
 {
     public class PowerUp : MonoBehaviour
@@ -13,32 +14,28 @@ namespace Project.Characters.Player.PlayerScripts.Combat
 
         private float currentMana;
         private bool isActive;
-        private Coroutine rechargeCoroutine;
+        
         public event Action<bool> OnPowerUpStateChanged;
         public event Action<float> OnManaChanged;
 
         public bool IsActive => isActive;
 
-        private void OnEnable()
+        void Start()
         {
-            ResetState();
+            currentMana = maxMana;
+            isActive = false;
+            OnManaChanged?.Invoke(GetManaNormalized());
         }
 
-        private void Update()
+        void Update()
         {
             HandleInput();
 
             if (isActive)
                 HandleManaConsumption();
-        }
-        private void ResetState()
-        {
-            currentMana = maxMana;
-            isActive = false;
-
-            OnManaChanged?.Invoke(GetManaNormalized());
-
-            StartRecharge();
+            
+            // Iniciar la coroutine de regeneración constantemente (como en PlayerDodge)
+            StartCoroutine(RegenMana());
         }
 
         private void HandleInput()
@@ -48,16 +45,17 @@ namespace Project.Characters.Player.PlayerScripts.Combat
                 ActivatePowerUp();
             }
         }
+
         private void HandleManaConsumption()
         {
             currentMana -= drainSpeed * Time.deltaTime;
-
+            
             if (currentMana <= 0)
             {
                 currentMana = 0;
                 DeactivatePowerUp();
             }
-
+            
             OnManaChanged?.Invoke(GetManaNormalized());
         }
 
@@ -65,45 +63,25 @@ namespace Project.Characters.Player.PlayerScripts.Combat
         {
             isActive = true;
             OnPowerUpStateChanged?.Invoke(true);
-            StopRecharge();
         }
 
         private void DeactivatePowerUp()
         {
             isActive = false;
             OnPowerUpStateChanged?.Invoke(false);
-            StartRecharge();
         }
 
-        private IEnumerator RegenRoutine()
+        private IEnumerator RegenMana()
         {
-            while (!isActive && currentMana < maxMana)
+            // Solo regenerar si NO está activo y la mana NO está llena
+            if (!isActive && currentMana < maxMana)
             {
                 currentMana += regenValue * Time.deltaTime;
                 currentMana = Mathf.Min(currentMana, maxMana);
-
+                
                 OnManaChanged?.Invoke(GetManaNormalized());
-
+                
                 yield return new WaitForSeconds(regenTime);
-            }
-
-            rechargeCoroutine = null;
-        }
-
-        private void StartRecharge()
-        {
-            if (rechargeCoroutine != null)
-                StopCoroutine(rechargeCoroutine);
-
-            rechargeCoroutine = StartCoroutine(RegenRoutine());
-        }
-
-        private void StopRecharge()
-        {
-            if (rechargeCoroutine != null)
-            {
-                StopCoroutine(rechargeCoroutine);
-                rechargeCoroutine = null;
             }
         }
 
